4a8c93f592be17adb0b562032ee6f34dcac6c41d
unidwell <gregor.mirai@gmail.com>
2024-11-05 16:42:23 +0100

    feat: export EAS.

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 import EAS from './src/eas.js'
-
-const words = EAS.generateMnemonic()
-console.log(words.join(' '))
-
-const privateKey = EAS.getPrivateKeyFromMnemonic(words)
-console.log(EAS.getPrivateKeyFromMnemonic(words))
-
-const address = EAS.getAddressFromPrivateKey(privateKey)
-console.log(address)
+export { EAS }
