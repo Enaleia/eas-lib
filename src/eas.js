633633a01912b7985ef54578641b09237daf3850
unidwell <gregor.mirai@gmail.com>
2024-11-07 15:31:13 +0100

    feat: add a schema registry method.

--- src/eas.js
+++ src/eas.js
@@ -1,10 +1,21 @@
+require('dotenv').config()
 const bip39 = require('bip39')
 const { Wallet, ethers } = require('ethers')
+const { EAS, SchemaEncoder, SchemaRegistry } = require('@ethereum-attestation-service/eas-sdk')
 
 /**
  * Class containing methods to work with the EAS protocol.
  */
-class EAS {
+class EASHelper {
+  /**
+   * @param {string} providerUrl - The RPC URL of the Ethereum provider.
+   * @param {string} privateKey - The private key of the wallet that will deploy the schema or send/get attestations.
+   */
+  constructor(providerUrl, privateKey) {
+    this.providerUrl = providerUrl
+    this.privateKey = privateKey
+  }
+
   /**
    * Generates a 12-word mnemonic phrase (128 bits of entropy).
    *
@@ -62,6 +73,46 @@ class EAS {
       return null
     }
   }
+
+  /**
+   * Register an EAS schema with the EAS registry.
+   *
+   * Possible failure reasons:
+   * - Invalid schema definition.
+   * - Similar schema definition already exists.
+   * - Insufficient gas.
+   * - Network connectivity issues.
+   * - Invalid private key.
+   * - Invalid provider URL.
+   *
+   * @param {string} schema - The schema definition string.
+   * @returns {Promise<string>} - The transaction hash of the schema registration.
+   */
+  async registerSchema(schema) {
+    try {
+      // Signer
+      const provider = new ethers.JsonRpcProvider(this.providerUrl)
+      const signer = new ethers.Wallet(this.privateKey, provider)
+
+      // Registry
+      const registryContractAddress = '0x4200000000000000000000000000000000000020'
+      const schemaRegistry = new SchemaRegistry(registryContractAddress)
+      schemaRegistry.connect(signer)
+
+      // Register schema
+      const tx = await schemaRegistry.register({
+        schema,
+        resolverAddress: '0x0000000000000000000000000000000000000000', // No resolver by default!
+        revocable: true,
+      })
+
+      const schemaUID = await tx.wait()
+      return schemaUID
+    } catch (error) {
+      console.error('Error registering schema:', error)
+      throw error
+    }
+  }
 }
 
-module.exports = { EAS }
+module.exports = { EASHelper }
