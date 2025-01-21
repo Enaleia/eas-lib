01f175dc673efc2affe0d6412feb1779aa5153fc
Chiali <info@chialitsai.com>
2025-01-21 18:57:19 +0700

    Update schema (#9)
    -adding one more field
    - reorder and rename fields

--- examples/register-complex-schema.js
+++ examples/register-complex-schema.js
@@ -10,11 +10,11 @@ async function main() {
 
   // Registering the same schema twice will fail.
   const schema =
-    'string userId, string companyName, string companyCoordinates,' +
-    'string eventTimestamp, string[] eventCoordinates, string actionName,' +
-    'string collectorName, string[] inputIdentifier, string[] inputMaterial,' +
-    'uint256[] inputWeight, string[] outputIdentifier, string[] outputMaterial,' +
-    'uint256[] outputWeight, string productName, uint256 batchQuantity'
+    'string userID, string portOrCompanyName, string[] portOrCompanyCoordinates,' +
+    'string actionType, string actionDate, string[] actionCoordinates, string collectorName,' +
+    'string[] incomingMaterials, uint256[] incomingWeightsKg, string[] incomingCodes,' +
+    'string[] outgoingMaterials,  uint256[] outgoingWeightsKg, string[] outgoingCodes, ' +
+    'string productName, uint256 batchQuantity, string weightPerItemKg'
 
   try {
     const timerStart = Date.now()
