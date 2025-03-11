6e69ef448de74b516eca803f7f85ae56d879cd8f
unidwell <gregor.mirai@gmail.com>
2025-03-11 20:51:04 +0800

    feat: promote schema and schemaUID to instance vars.

--- src/eas.js
+++ src/eas.js
@@ -18,10 +18,14 @@ class EASHelper {
   /**
    * @param {string} providerUrl - The RPC URL of the Ethereum provider.
    * @param {string} privateKey - The private key of the wallet that will deploy the schema or send/get attestations.
+   * @param {string} schema - The schema definition string.
+   * @param {string} schemaUID - The schema UID.
    */
-  constructor(providerUrl, privateKey) {
+  constructor(providerUrl, privateKey, schema, schemaUID) {
     this.providerUrl = providerUrl
     this.privateKey = privateKey
+    this.schema = schema
+    this.schemaUID = schemaUID
   }
 
   /**
@@ -95,10 +99,9 @@ class EASHelper {
    * - Invalid private key.
    * - Invalid provider URL.
    *
-   * @param {string} schema - The schema definition string.
    * @returns {Promise<string>} - The transaction hash of the schema registration.
    */
-  async registerSchema(schema) {
+  async registerSchema() {
     // Signer
     const provider = new ethers.JsonRpcProvider(this.providerUrl)
     const signer = new ethers.Wallet(this.privateKey, provider)
@@ -109,7 +112,7 @@ class EASHelper {
 
     // Register schema
     const tx = await schemaRegistry.register({
-      schema,
+      schema: this.schema,
       resolverAddress: '0x0000000000000000000000000000000000000000', // No resolver by default!
       revocable: true,
     })
@@ -180,36 +183,32 @@ class EASHelper {
   /**
    * Publish data to the EAS attestation contract.
    *
-   * @param {string} providerUrl - The RPC URL of the Ethereum provider.
-   * @param {string} privateKey - The private key of the wallet that will deploy the schema or send/get attestations
-   * @param {string} schema - The schema definition string.
-   * @param {string} schemaUID - The schema UID.
    * @param {object} data - The data to be attested.
    */
-  async attest(providerUrl, privateKey, schema, schemaUID, data) {
+  async attest(data) {
     const easContractAddress = '0x4200000000000000000000000000000000000021'
     const eas = new EAS(easContractAddress)
 
     // Initialize signer.
-    const provider = new ethers.JsonRpcProvider(providerUrl)
-    const signer = new ethers.Wallet(privateKey, provider)
+    const provider = new ethers.JsonRpcProvider(this.providerUrl)
+    const signer = new ethers.Wallet(this.privateKey, provider)
 
     // Signer must be an ethers-like signer.
     await eas.connect(signer)
 
     // Prepare data for encoding.
-    const { types: schemaTypes } = EASHelper.getSchemaTypesAndKeys(schema)
+    const { types: schemaTypes } = EASHelper.getSchemaTypesAndKeys(this.schema)
     const schemaData = []
     for (const [name, value] of Object.entries(data)) {
       schemaData.push({ name, value, type: schemaTypes[name] })
     }
 
     // Initialize SchemaEncoder with the schema string
-    const schemaEncoder = new SchemaEncoder(schema)
+    const schemaEncoder = new SchemaEncoder(this.schema)
     const encodedData = schemaEncoder.encodeData(schemaData)
 
     const tx = await eas.attest({
-      schema: schemaUID,
+      schema: this.schemaUID,
       data: {
         recipient: '0x0000000000000000000000000000000000000000',
         expirationTime: NO_EXPIRATION,
@@ -218,7 +217,6 @@ class EASHelper {
       },
     })
     const newAttestationUID = await tx.wait()
-    console.log('New attestation UID:', newAttestationUID)
     return newAttestationUID
   }
 }
